d87a0603777b0449d8220f129dc6c3b985829e83
janiomoscote <95260369+janiomoscote@users.noreply.github.com>
2026-03-17 20:54:00 -0500

    Corrección del profesor

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
 
-export default nextConfig;
+module.exports = {
+    images: {
+        domains: ['www.freetogame.com'],
+    },
+}
+
--- src/app/juegos/[id]/page.jsx
+++ src/app/juegos/[id]/page.jsx
@@ -1,4 +1,4 @@
-
+import Link from "next/link";
 
 async function getGames(id) {
     const res =  await fetch(`https://www.freetogame.com/api/games/${id}`)
@@ -10,7 +10,7 @@ async function Juegos({params}: any) {
     const juego = await getGames(params.id)
     return (
         <div className="bg-slate-200 p-10 rounded-md">
-            <img src={juego.thumbnail} alt={juego.title} className="m-auto my-4"/>
+            <Link src={juego.thumbnail} alt={juego.title} className="m-auto my-4"/>
             <h3 className="text-3xl font-bold">{juego.id} {juego.title}</h3>
             <p> genero: {juego.genre} </p>
         </div>
